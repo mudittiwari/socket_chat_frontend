91732be1d491a3e199484160378c8b724064b65a
mudit <mudit19.072@gmail.com>
2024-08-17 19:38:08 +0530

    frontend started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 
 import './App.css';
 import { BrowserRouter,HashRouter ,Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Chat from './Chat';
 function App() {
 
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </HashRouter>
+    // <HashRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //   </Routes>
+    // </HashRouter>
+    <h1>hello world</h1>
   );
 }
 
